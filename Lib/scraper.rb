1b2585c35696df8b4931908bf83b610abd1b9f8e
CodeOatmeal <JahmilEady@gmail.com>
2013-11-21 16:39:25 -0500

    update code

--- Lib/scraper.rb
+++ Lib/scraper.rb
@@ -30,13 +30,11 @@ class Scraper
 
 
   def get_blog_url
-    new_array = []
-   
-      end
+    blog_url = html.search(".blog:href")
   end
 
   def get_twitter_handle
-     new_array = []
+    new_array = []
     twitter_handle = html.search("li a").text.split(" ")
     twitter_handle.each do |element|
       if element[0] == "@"
@@ -49,3 +47,18 @@ puts my_scraper.download.html.inspect
 
 
 #html element attributes - you need to find out how to access twitter user name 
+
+my_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
+#puts my_scraper.html
+#puts my_scraper.get_students_names
+#puts my_scraper.get_twitter_handle
+puts my_scraper.get_blog_url
+
+
+#we call "nokogiri" is a class, "HTML" class method is inside, we pass download to HTML method
+#HTML is a method that makes the file #<File:0x007fcb04057dd8> pretty and usable in ruby
+# class::class_method
+# class::constant
+#we store the return value of the method open in the var download
+#we keep it local because we're going to use it to create html instance var
+#.search is a nokogiri method
